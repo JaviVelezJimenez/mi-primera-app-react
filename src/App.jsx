48135ae2f246da6dc4f369021c8fafab8b711c80
Javier <javier_velez@outlook.com>
2025-03-23 16:19:52 +0100

    Se eliminan los comentarios

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
-//Esto es un commint desde el visual estudio sin comandos
 import { useState } from 'react';
 
-// Lista de anécdotas
 const anecdotes = [
   'Si no puedes hacerlo bien, al menos haz que luzca bien.',
   'La depuración es como ser el detective de una novela escrita por ti mismo.',
@@ -12,19 +10,15 @@ const anecdotes = [
 ];
 
 function App() {
-  // Estado para saber cuál anécdota se está mostrando (posición en el array)
   const [indiceActual, setIndiceActual] = useState(0);
 
-  // Estado para guardar cuántos votos tiene cada anécdota (array de números)
   const [votos, setVotos] = useState(new Array(anecdotes.length).fill(0));
 
-  // Cambia a una anécdota aleatoria
   const siguienteAnecdota = () => {
     const aleatorio = Math.floor(Math.random() * anecdotes.length);
     setIndiceActual(aleatorio);
   };
 
-  // Suma un voto a la anécdota actual
   const votar = () => {
     const nuevosVotos = [...votos]; // copia del array original
     nuevosVotos[indiceActual] += 1; // suma 1 al voto actual
